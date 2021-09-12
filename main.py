ccd2aba3a1a06360e36e70da260c1223d296d01a
Laktionof <68972608+Laktionof@users.noreply.github.com>
2021-09-12 15:59:44 +0300

    Hi-load fix
    Now the script can work with a lot of media at the same time

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
 from pyrogram import Client, filters
-import time
-import os
-from config import session_name, api_id, api_hash, channel_id
+from time import sleep, strftime, gmtime, time
+from os import remove
+from os.path import join
+from config import *
 
 app = Client(session_name, api_id, api_hash)
 
 
-@app.on_message((filters.photo | filters.video) & filters.private & ~filters.me)
+@app.on_message(filters.private & ~filters.me & (filters.photo | filters.video))
 def ttl_download(_, msg):
-    full_name = msg.from_user.first_name
-    if msg.from_user.last_name:
-        full_name += " " + msg.from_user.last_name
-
+    att_type = 2  # 0 - photo, 1 - video
+    filetype = ("jpg", "mp4")
     if hasattr(msg.photo, "ttl_seconds"):
         if msg.photo.ttl_seconds:
-            app.download_media(msg, "photo.jpg")
-            mention = f"[{full_name}](tg://user?id={msg.from_user.id}), " \
-                      f"{time.strftime('%d.%m.%Y %H:%M:%S', time.gmtime(msg.date))}, {msg.photo.ttl_seconds}s"
-            with open("downloads/photo.jpg", "rb") as photo:
-                app.send_photo(channel_id, photo, mention)
-            os.remove(os.path.join("downloads", "photo.jpg"))
-
-    if hasattr(msg.video, "ttl_seconds"):
+            att_type = 0
+    elif hasattr(msg.video, "ttl_seconds"):
         if msg.video.ttl_seconds:
-            app.download_media(msg, "video.mp4")
-            mention = f"[{full_name}](tg://user?id={msg.from_user.id}), " \
-                      f"{time.strftime('%d.%m.%Y %H:%M:%S', time.gmtime(msg.date))}, {msg.video.ttl_seconds}s"
-            with open("downloads/video.mp4", "rb") as video:
-                app.send_video(channel_id, video, mention)
-            os.remove(os.path.join("downloads", "video.mp4"))
+            att_type = 1
+
+    if att_type != 2:
+        full_name = msg.from_user.first_name
+        if msg.from_user.last_name:
+            full_name += " " + msg.from_user.last_name
+
+        file_name = f"{time()}.{filetype[att_type]}"
+        app.download_media(msg, file_name)
+        mention = f"[{full_name}](tg://user?id={msg.from_user.id}), " \
+                  f"{strftime('%d.%m.%Y %H:%M:%S', gmtime(msg.date))}, {msg.photo.ttl_seconds}s"
+        with open(join("downloads", file_name), "rb") as att:
+            if att_type == 0:
+                app.send_photo(channel_id, att, mention)
+            elif att_type == 1:
+                app.send_video(channel_id, att, mention)
+        remove(join("downloads", file_name))
 
 
 app.run()
