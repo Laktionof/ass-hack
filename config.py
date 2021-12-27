2eb13fc382cb30c433b4ddbfd8656dec43344ece
Laktionof <68972608+Laktionof@users.noreply.github.com>
2021-12-27 10:38:07 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-session_name = "someuser"
-api_id = 1523785
-api_hash = "0dabob00ecd62db51b8d4136b09e32cd"
-channel_id = -1001126514342
+session_name = "example"
+api_id = 1234567
+api_hash = "123abcd123abcd123abcd"
+channel_id = -1001234567890
 
 last_messages_amount = 50
